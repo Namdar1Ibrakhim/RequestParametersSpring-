a11d66d5739e6c724ffe41364da14059e5abedca
Ibrakhim <113717353+Namdar1Ibrakhim@users.noreply.github.com>
2023-01-01 17:05:57 +0500

    Add files via upload

---
+++ src/main/java/com/example/paramentryzaprosa/ParamentryZaprosaApplication.java
@@ -0,0 +1,13 @@
+package com.example.paramentryzaprosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamentryZaprosaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParamentryZaprosaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paramentryzaprosa/ParamentryZaprosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paramentryzaprosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamentryZaprosaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
